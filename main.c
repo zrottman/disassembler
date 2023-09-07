0df3d300a495297604c5d8060bf234ef28e65b3c
zrottman <zrottman@example.com>
2023-09-07 11:03:05 -0700

    Successfully parse instruction template types and print single-length instructions (templates A, B, C, D)

--- main.c
+++ main.c
@@ -4,7 +4,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-typedef enum Template { A, B, C, D, A2, B2, C2, E2, A3, B3, E3, VACANT } Template;
+typedef enum Template { A, B, C, D, A2, B2, C2, E2, A3, B3, E3, VACANT, JUMP_TODO } Template;
 
 uint8_t read_len(uint8_t *instruction)
 {
@@ -18,21 +18,27 @@ uint8_t read_mode1(uint8_t *instruction)
     return (mode1_mask & instruction[3]) >> 3;
 }
 
-uint8_t read_operand_6(uint8_t *instruction)
+uint8_t read_opcode_6(uint8_t *instruction)
 {
     uint8_t mask_1 = 0b00000111;
     uint8_t mask_2 = 0b11100000;
     return ((instruction[3] & mask_1) << 3 | (instruction[2] & mask_2) >> 5);
 }
 
-void print_b(uint8_t *instruction)
+uint8_t read_m_bit(uint8_t *instruction)
 {
-	uint8_t mask;
-	int output;
+	uint8_t mask = 0b10000000;
+	return (instruction[1] & mask) >> 7;
+}
+
+void print_a(uint8_t *instruction)
+{
+    uint8_t mask;
+    int output;
 
 	printf("instruction length: %d\n", read_len(instruction));
 	printf("mode: %d\n", read_mode1(instruction));
-	printf("operand: %d\n", read_operand_6(instruction));
+	printf("opcode: %d\n", read_opcode_6(instruction));
 
 	mask = 0b00011111;
 	output = (instruction[2] & mask);
@@ -48,11 +54,87 @@ void print_b(uint8_t *instruction)
 
 	mask = 0b00011111;
 	output = (instruction[1] & mask);
-	printf("src register: %d\n", output);
+	printf("src register S: %d\n", output);
+
+    mask = 0b11100000;
+    output = instruction[0] & mask >> 5;
+    printf("mask: %d\n", output);
+
+    mask = 0b00011111;
+	output = instruction[0] & mask;
+	printf("source register T: %d\n", output);
+}
+
+void print_b(uint8_t *instruction)
+{
+	uint8_t mask;
+	int output;
+
+	printf("instruction length: %d\n", read_len(instruction));
+	printf("mode: %d\n", read_mode1(instruction));
+	printf("opcode: %d\n", read_opcode_6(instruction));
+
+	mask = 0b00011111;
+	output = (instruction[2] & mask);
+	printf("dest register: %d\n", output);
+
+	mask = 0b10000000;
+	output = instruction[1] & mask >> 7;
+	printf("M: %d\n", output);
+
+	mask = 0b01100000;
+	output = instruction[1] & mask >> 5;
+	printf("operand type: %d\n", output);
+
+	mask = 0b00011111;
+	output = (instruction[1] & mask);
+	printf("src register S: %d\n", output);
 
 	output = instruction[0];
 	printf("immediate constant: %d\n", output);
 }
+
+void print_c(uint8_t *instruction)
+{
+    uint8_t mask;
+    int output;
+
+    printf("instruction length: %d\n", read_len(instruction));
+    printf("mode: %d\n", read_mode1(instruction));
+    printf("opcode: %d\n", read_opcode_6(instruction));
+
+    mask = 0b00011111;
+    output = (instruction[2] & mask);
+    printf("dest register: %d\n", output);
+
+    output = instruction[1];
+    printf("immediate constant 2: %d\n", output);
+
+    output = instruction[0];
+    printf("immediate constant 1: %d\n", output);
+}
+
+void print_d(uint8_t *instruction)
+{
+    uint8_t mask;
+    int output;
+
+    printf("instruction length: %d\n", read_len(instruction));
+    printf("mode: %d\n", read_mode1(instruction));
+
+    mask = 0b00000111;
+    output = instruction[3] & mask;
+    printf("opcode: %d\n", output);
+
+    output = instruction[2];
+    output <<= 8;
+    output |= instruction[1];
+    output <<= 8;
+    output |= instruction[0];
+    
+    printf("immediate constant 3: %d\n", output);
+}
+
 
 
 Template get_template_0(uint8_t *instruction)
@@ -75,8 +157,8 @@ Template get_template_0(uint8_t *instruction)
         case 7:
             return B;
         default:
-	       printf("error\n");
-            exit(1);
+	       printf("error 0\n");
+           exit(1);
     }
 }
 
@@ -96,11 +178,84 @@ Template get_template_1(uint8_t *instruction)
         case 5:
             return VACANT;
         case 6:
-            return A;
+            return JUMP_TODO;
         case 7:
-            return B;
+            if ((0 <= read_opcode_6(instruction)) && (read_opcode_6(instruction) < 16)) {
+                return D;
+            }
+            return C;
         default:
-	       printf("error\n");
+	       printf("error 1\n");
+            exit(1);
+    }
+}
+
+Template get_template_2(uint8_t *instruction)
+{
+    switch (read_mode1(instruction)) {
+        case 0:
+            switch (read_m_bit(instruction)) {
+                case 0:
+                    return E2;
+                case 1:
+                    return A2;
+            }
+        case 1:
+            return A2;
+        case 2:
+            return E2;
+        case 3:
+            return A2;
+        case 4:
+            return A2;
+        case 5:
+            switch (read_opcode_6(instruction)) {
+                case 3:
+                    return A2;
+                case 1:
+                case 2:
+                    return B2;
+                case 4:
+                case 5:
+                case 7:
+                    return C2;
+            }
+        case 6:
+            return A2;
+        case 7:
+            return VACANT;
+        default:
+	        printf("error 2\n");
+            exit(1);
+    }
+}
+
+Template get_template_3(uint8_t *instruction)
+{
+    switch (read_mode1(instruction)) {
+        case 0:
+            return E3;
+        case 1:
+            switch (read_opcode_6(instruction)) {
+                case 0:
+                    return A3;
+                case 1:
+                    return B3;
+            }
+        case 2:
+            return E3;
+        case 3:
+            return A3;
+        case 4:
+            return VACANT;
+        case 5:
+            return VACANT;
+        case 6:
+            return VACANT;
+        case 7:
+            return VACANT;
+        default:
+	        printf("error 3\n");
             exit(1);
     }
 }
@@ -113,25 +268,18 @@ Template get_template(uint8_t *instruction)
             return get_template_0(instruction);
         case 1:
             return get_template_1(instruction);
-//        case 2:
-//            return get_template_2(instruction);
-//        case 3:
-//            return get_template_3(instruction);
-        default:
-	       printf("error\n");
-            exit(1);
+        case 2:
+            return get_template_2(instruction);
+        case 3:
+            return get_template_3(instruction);
+        default: // shouldn't ever happen
+            return VACANT;
     }
 }
 
 int main() {
-    uint8_t instruction[] = {
-	    0b00000000,
-	    0b00011111,
-	    0b00011111,
-	    0b01011000,
-    };
 
-    int fd = open("input.ob", O_RDONLY);
+    int fd = open("memset.ob", O_RDONLY);
     if (fd < 0) {
 	    printf("open failed\n");
 	    exit(1);
@@ -144,8 +292,73 @@ int main() {
 	    exit(1);
     }
 
-    if (get_template(instruction) == B) {
-	    printf("great success!\n");
+    uint8_t *p = buf;
+
+    while (p < buf + n) {
+        for (int i=3; i>=0; --i) {
+            printf("%x ", p[i]);
+        }
+        printf("\n");
+        switch (get_template(p)) {
+            case A:
+                printf("template A\n");
+                print_a(p);
+                p += 4;
+                break;
+            case B:
+                printf("template B\n");
+                print_b(p);
+                p += 4;
+                break;
+            case C:
+                printf("template C\n");
+                print_c(p);
+                p += 4;
+                break;
+            case D:
+                printf("template D\n");
+                print_d(p);
+                p += 4;
+                break;
+            case A2:
+                printf("template A2\n");
+                p += 8;
+                break;
+            case B2:
+                printf("template B2\n");
+                p += 8;
+                break;
+            case C2:
+                printf("template C2\n");
+                p += 8;
+                break;
+            case E2:
+                printf("template E2\n");
+                p += 8;
+                break;
+            case A3:
+                printf("template A3\n");
+                p += 12;
+                break;
+            case B3:
+                printf("template B3\n");
+                p += 12;
+                break;
+            case E3:
+                printf("template E3\n");
+                p += 12;
+                break;
+            case JUMP_TODO:
+                printf("jump template 1.6A or 1.7B\n");
+                p += 4;
+                break;
+            default:
+                printf("error\n");
+                exit(1);
+        }
+        printf("\n");
     }
+
+
     return 0;
 }
